504ad9fd1522fa943fd07643472a82a480c3da74
kirbxe <alex.lantion06@mail.ru>
2025-10-21 01:21:04 +0500

    Create pagination

--- resources/js/Pages/products/CatalogProduct.vue
+++ resources/js/Pages/products/CatalogProduct.vue
@@ -2,42 +2,60 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
 import CartButton from '@/Components/CartButton.vue';
+import { computed } from 'vue';
 
 const props = defineProps({ products: Object })
 
+const pageKey = computed(() => props.products.current_page)
 </script>
 
 <template>
-
-    <Head title="Product" />
-
     <AuthenticatedLayout>
+        <Head title="Product" />
         <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 Product Catalog
             </h2>
         </template>
+        <div :key="pageKey">
+            <div class='py-12 flex justify-center font-[Roboto]'>
+                <Link :href="route('products.create')"><button
+                    class="border p-1 rounded-xl hover:bg-blue-500 border-black">Click
+                    for create</button></Link>
+            </div>
+            <div class='grid grid-rows-2 grid-cols-5 gap-y-[10px]  gap-x-[10px] px-12 font-[Roboto]'>
+                <div v-for="product in props.products.data" :key="product.id"
+                    class='w-[200px] h-[462px] flex flex-col justify-between mb-1 font-roboto'>
+                    <div class='rounded-[10px] p-2 w-full h-[270px]'>
+                        <img :src="product.picture1" alt="Неудалось загрузить изображение"> 
+                    </div>
+                    <div class="flex justify-start">
+                        здесь будет рейтинг
+                    </div>
+                    <span class="text-[18px] font-medium">{{ product.name }}</span>
+                    <div class='flex justify-between'>
+                        <span class='text-left '>{{ product.price }}₽</span>
+                        <CartButton />
+                    </div>
 
-        <div class='py-12 flex justify-center font-[Roboto]'>
-            <Link :href="route('products.create')"><button
-                class="border p-1 rounded-xl hover:bg-blue-500 border-black">Click
-                for create</button></Link>
-        </div>
-        <div class='flex justify-center gap-x-[10px] flex-wrap px-12 font-[Roboto]'>
-            <div v-for="product in props.products" :key="product.id" class='w-[200px] h-[462px] flex flex-col justify-between border border-[black] mb-1 font-roboto'>
-                <div class='rounded-[10px] p-2 w-full h-[270px]'> 
-                     <img src="{{ product.pricture1 }}" alt=""> 
                 </div>
-                <div class="flex justify-start">
-                    здесь будет рейтинг
+
+            </div>
+            <div class='flex flex-col justify-center'>
+                <div class='flex justify-center items-center mt-8 font-[Roboto]'>
+                    Показано {{ props.products.from }} по {{ props.products.to }} из {{ props.products.total }}
                 </div>
-                <span class="text-[18px] font-medium">{{ product.name }}</span>
-                   <div class='flex justify-between'>
-                   <span class='text-left '>{{ product.price }}₽</span> 
-                    <CartButton/>
-                </div> 
+                <div class='flex gap-2 justify-center'>
+                    <Link v-if="props.products.prev_page_url" :href="props.products.prev_page_url"
+                        class='px-4 py-4 border rounded-md border-[black] hover:bg-gray-300'>
+                    Назад
+                    </Link>
+                    <Link v-if="props.products.next_page_url" :href="props.products.next_page_url"
+                        class='px-4 py-4 border rounded-md border-[black] hover:bg-gray-300'>
+                    Вперед
+                    </Link>
+                </div>
             </div>
         </div>
-
     </AuthenticatedLayout>
 </template>
